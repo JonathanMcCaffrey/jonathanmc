9ddbde75c1dbd481d3887ce3eda5b8b40fecfb13
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2019-08-26 20:48:11 -0400

    Additional styling

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -7,40 +7,100 @@ import styled from 'styled-components';
 const showJobs = (data: ResumeDataQuery) => {
 	return data.jobs.edges.map(element => {
 		return (
-			<Block>
-				<Text>{element.node.frontmatter!.company}</Text>
-				<Text>{element.node.frontmatter!.period}</Text>
-				<Text>{element.node.frontmatter!.mainrole}</Text>
-				<Text>{element.node.frontmatter!.companyDetails}</Text>
-
-				<div></div>
-				<Text
-					dangerouslySetInnerHTML={{ __html: element.node.html! }}
-				/>
-			</Block>
+			<JobBlock>
+				<LeftJob>
+					<Text>{element.node.frontmatter!.period}</Text>
+				</LeftJob>
+				<RightJob>
+					<MainRole>{element.node.frontmatter!.mainrole}</MainRole>
+					<Company>{element.node.frontmatter!.company}, </Company>
+					<CompanyDetails>
+						{element.node.frontmatter!.companyDetails}
+					</CompanyDetails>
+				</RightJob>
+				<ContentJob>
+					<Text
+						dangerouslySetInnerHTML={{ __html: element.node.html! }}
+					/>
+				</ContentJob>
+			</JobBlock>
 		);
 	});
 };
 
 const ResumePage = (props: { data: ResumeDataQuery }) => {
-	return <Layout>{showJobs(props.data)}</Layout>;
+	return (
+		<Resume>
+			<Section>Experience</Section>
+			{showJobs(props.data)}
+		</Resume>
+	);
 };
 
 export default ResumePage;
 
-const Block = styled.div`
-	background-color: rgb(40, 40, 40);
-	border: 1px rgb(80, 80, 80) solid;
-	padding: 20px;
-	margin: 40px auto;
-	layout: block;
-	width: 80vw;
+const Resume = styled.div`
+	background-color: rgb(244, 244, 244);
+	width: 100%;
+
+	*,
+	*:before,
+	*:after {
+		color: black;
+	}
 `;
 
-const Text = styled.p`
-	color: white;
-	width: 100%;
+const Section = styled.div`
+	border-top: 1px solid black;
+	border-bottom: 1px solid black;
+	font-size: 1.2rem;
+	font-weight: 600;
 `;
+
+const MainRole = styled.div`
+	color: black;
+	font-size: 1.2rem;
+	font-weight: 600;
+`;
+
+const Company = styled.div`
+	color: rgb(40, 80, 40);
+	font-size: 1.1rem;
+	font-weight: 400;
+
+	display: inline-block;
+`;
+
+const CompanyDetails = styled.div`
+	display: inline-block;
+	padding-left: 5px;
+`;
+
+const JobBlock = styled.div`
+	padding: 20px;
+	display: grid;
+	grid-template-columns: 1fr 20px 8fr;
+	grid-template-rows: auto;
+	grid-template-areas:
+		'LeftJob . RightJob'
+		'ContentJob ContentJob ContentJob';
+`;
+
+const LeftJob = styled.div`
+	grid-area: LeftJob;
+`;
+
+const ContentJob = styled.div`
+	grid-area: ContentJob;
+	padding-top: 10px;
+	padding-left: 20px;
+`;
+
+const RightJob = styled.div`
+	grid-area: RightJob;
+`;
+
+const Text = styled.div``;
 
 export const pageQuery = graphql`
 	query Resume {
